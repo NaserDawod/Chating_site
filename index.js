a5da8dddad153523776b1762f17bf37f9816dede
hibashahbari <92367215+hibashahbari@users.noreply.github.com>
2022-04-03 14:10:48 +0300

    4/3 new

--- index.js
+++ index.js
@@ -36,15 +36,29 @@ contacts.push(new contact("bhem3", "123"))
 contacts.push(new contact("bhem4", "123"))
 contacts.push(new contact("bhem5", "123"))
 
+// contacts.forEach(elem =>{
+//     document.write("<a href=\"#\" class=\"list-group-item list-group-item-action\">" +
+//                     "<div class=\"d-flex w-100 justify-content-between\">" +
+//                     "<h5 class=\"mb-1\">" + elem.getname() + "</h5>" +
+//                     "<small class=\"text-muted\">3 days ago</small>" +
+//                     "</div>"+ 
+//                     "<p class=\"mb-1\">message</p>" +
+//                     "</a>")
+// });
+
 contacts.forEach(elem =>{
-    document.write("<a href=\"#\" class=\"list-group-item list-group-item-action\">" +
-                    "<div class=\"d-flex w-100 justify-content-between\">" +
-                    "<h5 class=\"mb-1\">" + elem.getname() + "</h5>" +
-                    "<small class=\"text-muted\">3 days ago</small>" +
-                    "</div>"+ 
-                    "<p class=\"mb-1\">message</p>" +
+    document.write("<a href=\"LogInPage.html\" class=\"list-group-item list-group-item-action\">" + 
+                    "<div class=\"d-flex justify-content-between\">" +
+                        "<img src=\"images.jpg\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:40px;\">" +
+                            "<div class=\"placeee\">" + 
+                                "<h4 class=\"mb-1\">bhem2</h4>" + 
+                                "<p class=\"mb-1\">message</p>" +
+                                "</div>" + 
+                            "<small class=\"text-muted\">3 days ago</small>" + 
+                            "</div>" +
                     "</a>")
 });
+
 
 
 
