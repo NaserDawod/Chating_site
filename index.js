cd2243d2d3d2b11ee5eedc72b23bd7695a4eb35f
NaserDawod <90802273+NaserDawod@users.noreply.github.com>
2022-04-06 10:53:34 +0300

    fixed

--- index.js
+++ index.js
@@ -54,16 +54,16 @@ contacts.push(new contact("bhem5", "123", "images.jpg"))
 
 function printContacts(contacts) {
     contacts.forEach(elem =>{
-        document.write("<a href=\"\" class=\"list-group-item list-group-item-action\">" + 
-                            "<div class=\"d-flex justify-content-between\">" +
-                                "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:40px;\">" +
-                                "<div class=\"placeee\">" + 
-                                    "<h6 class=\"mb-1\">" + elem.getname() + "</h6>" + 
-                                    "<p class=\"mb-1\">message</p>" +
-                                "</div>" + 
-                                "<small class=\"text-muted\">3 days ago</small>" + 
+        document.write("<div class=\"chat-list-item d-flex flex-row w-100 p-2 border-bottom\" onclick=\"showMessages()\">" + 
+                            "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:50px;\">" +
+                            "<div class=\"w-50\">" +
+                                "<h5 class=\"mb-1\">" + elem.getname() + "</h5>" +
+                                "<p class=\"mb-1\">message</p>" +
                             "</div>" +
-                        "</a>")
+                            "<div class=\"flex-grow-1 text-right\">" +
+                                "<div class=\"small time\">15:41</div>" + 
+                            "</div>" +
+                        "</div>")
     });
 }
 
@@ -76,19 +76,36 @@ function addContact(contacts){
     let elem = document.getElementById('contact_div')
     str = ""
     contacts.forEach(elem =>{
-        str += ("<a href=\"\" class=\"list-group-item list-group-item-action\">" + 
-                    "<div class=\"d-flex justify-content-between\">" +
-                        "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:40px;\">" +
-                        "<div class=\"placeee\">" + 
-                            "<h6 class=\"mb-1\">" + elem.getname() + "</h6>" + 
-                            "<p class=\"mb-1\">message</p>" +
-                        "</div>" + 
-                        "<small class=\"text-muted\">3 days ago</small>" + 
-                    "</div>" +
-                "</a>")
+        str += ("<div class=\"chat-list-item d-flex flex-row w-100 p-2 border-bottom\">" + 
+                            "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:50px;\">" +
+                            "<div class=\"w-50\">" +
+                                "<h5 class=\"mb-1\">" + elem.getname() + "</h5>" +
+                                "<p class=\"mb-1\">message</p>" +
+                            "</div>" +
+                            "<div class=\"flex-grow-1 text-right\">" +
+                                "<div class=\"small time\">15:41</div>" + 
+                            "</div>" +
+                        "</div>")
     });
     elem.innerHTML = "<table class=\"table table-bordered table-striped mb-0\">" + 
                         str +
                      "</table>"
 }
 
+
+function showMessages(){
+    let elem = document.getElementById('messages')
+    // elem.innerHTML = 
+}
+
+
+// "<a href=\"\" class=\"list-group-item list-group-item-action\">" + 
+//                     "<div class=\"d-flex justify-content-between\">" +
+//                         "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:40px;\">" +
+//                         "<div class=\"placeee\">" + 
+//                             "<h6 class=\"mb-1\">" + elem.getname() + "</h6>" + 
+//                             "<p class=\"mb-1\">message</p>" +
+//                         "</div>" + 
+//                         "<small class=\"text-muted\">3 days ago</small>" + 
+//                     "</div>" +
+//                 "</a>"
