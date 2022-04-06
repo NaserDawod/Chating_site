3a7705dbf257b10714dea5c7b1b637f49ff9e3ba
NaserDawod <90802273+NaserDawod@users.noreply.github.com>
2022-04-06 16:06:14 +0300

    asa

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ function showMessages(i){
                         "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-three-dots-vertical\" viewBox=\"0 0 16 16\">" + 
                             "<path d=\"M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z\"/>" +
                         "</svg>" +
+                    "</div>" +
+                    "<div class=\"d-flex flex-column chat-de w-100 h-100 overlay\" id=\"\">" +
                     "</div>"
 }
 
