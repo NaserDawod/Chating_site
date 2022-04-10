57678e84cf82845505fbde8552839e6656d65096
NaserDawod <90802273+NaserDawod@users.noreply.github.com>
2022-04-10 13:06:18 +0300

    fix

--- index.js
+++ index.js
@@ -47,41 +47,6 @@ contacts_map["bhem8"] = new contact("bhem8", "images.jpg")
 contacts_map["bhem9"] = new contact("bhem9", "images.jpg")
 contacts_map["bhem10"] = new contact("bhem10", "images.jpg")
 
-// contacts.push(new contact("bhem1", "images.jpg"))
-// contacts.push(new contact("bhem2", "images.jpg"))
-// contacts.push(new contact("bhem3", "images.jpg"))
-// contacts.push(new contact("bhem4", "images.jpg"))
-// contacts.push(new contact("bhem5", "images.jpg"))
-// contacts.push(new contact("bhem1", "images.jpg"))
-// contacts.push(new contact("bhem2", "images.jpg"))
-// contacts.push(new contact("bhem3", "images.jpg"))
-// contacts.push(new contact("bhem4", "images.jpg"))
-// contacts.push(new contact("bhem5", "images.jpg"))
-// contacts.push(new contact("bhem1", "images.jpg"))
-// contacts.push(new contact("bhem2", "images.jpg"))
-// contacts.push(new contact("bhem3", "images.jpg"))
-// contacts.push(new contact("bhem4", "images.jpg"))
-// contacts.push(new contact("bhem5", "images.jpg"))
-
-// contacts.forEach(elem =>{
-//     document.write("<a href=\"#\" class=\"list-group-item list-group-item-action\">" +
-//                     "<div class=\"d-flex w-100 justify-content-between\">" +
-//                     "<h5 class=\"mb-1\">" + elem.getname() + "</h5>" +
-//                     "<small class=\"text-muted\">3 days ago</small>" +
-//                     "</div>"+ 
-//                     "<p class=\"mb-1\">message</p>" +
-//                     "</a>")
-// });
-
-// var message = "<div class=\"align-self-end self p-1 my-1 mx-3 rounded bg-white shadow-sm message-item\">"
-//                     "<div class=\"d-flex flex-row\">" +
-//                         "<div class=\"body m-1 mr-2\">if you go to the movie, then give me a call</div>" +
-//                         "<div class=\"time ml-auto small text-right flex-shrink-0 align-self-end text-muted\" style=\"width:75px;\">" +
-//                             "22:41" +
-//                             "<i class=\"fas fa-check-circle\"></i>" +
-//                         "</div>" +
-//                     "</div>" +
-//                "</div>"
 
 function readMessage(key){
     let elem = contacts_map[key].getmessages()
@@ -190,14 +155,14 @@ function sendMessage() {
     }else{
         let elem = document.getElementById('chat_p')
         elem.innerHTML += "<div class=\"align-self-end self p-1 my-1 mx-3 rounded shadow-sm message-item greenbackground\">"+          
-                        "<div class=\"d-flex flex-row\">"+
-                            "<div class=\"body m-1 mr-2\">"+ message +"</div>"+
-                            "<div class=\"time ml-auto small text-right flex-shrink-0 align-self-end text-muted\" style=\"width:75px;\">"+
-                                time +
-                                "<i class=\"fas fa-check-circle\"></i>"+
+                            "<div class=\"d-flex flex-row\">"+
+                                "<div class=\"body m-1 mr-2\">"+ message +"</div>"+
+                                "<div class=\"time ml-auto small text-right flex-shrink-0 align-self-end text-muted\" style=\"width:75px;\">"+
+                                    time +
+                                    "<i class=\"fas fa-check-circle\"></i>"+
+                                "</div>"+
                             "</div>"+
-                        "</div>"+
-                    "</div>"
+                        "</div>"
         document.getElementById(name+'-t').innerText = time
         document.getElementById(name+'-m').innerText = message
     }
@@ -217,19 +182,6 @@ function printContacts(contacts) {
                             "</div>" +
                         "</div>")
     }
-    // contacts.forEach(elem =>{
-    //     document.write("<div class=\"chat-list-item d-flex flex-row w-100 p-2 border-bottom\" onclick=\"showMessages("+ i +")\">" + 
-    //                         "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:50px;\">" +
-    //                         "<div class=\"w-50\">" +
-    //                             "<h5 class=\"mb-1\">" + elem.getname() + "</h5>" +
-    //                             "<p class=\"mb-1\">message</p>" +
-    //                         "</div>" +
-    //                         "<div class=\"flex-grow-1 text-right\">" +
-    //                             "<div class=\"small time\">15:41</div>" + 
-    //                         "</div>" +
-    //                     "</div>")
-    //     i += 1
-    // });
 }
 
 function addContact(contacts){
@@ -282,24 +234,3 @@ function addContact(contacts){
 
 // } 
 
-// "<div class=\"bg-light\">" + 
-//                         "<img src=\"images.jpg\" class=\"profileimage\">" +
-//                         "<span clas=\"d-flex\">" + "bheadasd" + "</span>" +
-//                             "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-search\" viewBox=\"0 0 16 16\">" +
-//                                 "<path d=\"M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z\"/>" +
-//                             "</svg>" +
-//                         "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-three-dots-vertical\" viewBox=\"0 0 16 16\">" + 
-//                             "<path d=\"M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z\"/>" +
-//                         "</svg>" +
-//                     "</div>"
-
-// "<a href=\"\" class=\"list-group-item list-group-item-action\">" + 
-//                     "<div class=\"d-flex justify-content-between\">" +
-//                         "<img src=\"" + elem.getimg() + "\" alt=\"Profile Photo\" class=\"img-fluid rounded-circle mr-2\" style=\"height:40px;\">" +
-//                         "<div class=\"placeee\">" + 
-//                             "<h6 class=\"mb-1\">" + elem.getname() + "</h6>" + 
-//                             "<p class=\"mb-1\">message</p>" +
-//                         "</div>" + 
-//                         "<small class=\"text-muted\">3 days ago</small>" + 
-//                     "</div>" +
-//                 "</a>"
