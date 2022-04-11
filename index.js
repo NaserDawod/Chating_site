801788cc6a4dcb01d14400c5f6cff9417000fa48
hibashahbari <92367215+hibashahbari@users.noreply.github.com>
2022-04-11 17:01:01 +0300

    hjkol

--- index.js
+++ index.js
@@ -111,9 +111,9 @@ function showMessages(i){
                                 "<path d=\"M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z\"/>" +
                             "</svg>" + 
                             "<div class=\"dropup-content2\">"+
-                                "<div>aa</div>"+
-                                "<div>aa</div>"+
-                                "<div>aa</div>"+
+                                "<div class=\"biclips2\">aa</div>"+
+                                "<div class=\"biclips2\">aa</div>"+
+                                "<div class=\"biclips2\">aa</div>"+
                             "</div>"+
                         "</button>"+
                         "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-three-dots-vertical\" viewBox=\"0 0 16 16\">" + 
